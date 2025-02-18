f72008afd160f6e92b14a5133f47b54f23c700f0
Branislav <brano.obrtac@gmail.com>
2025-02-18 19:57:35 +0100

    added more params for input field

--- src/components/form/inputs/Input.tsx
+++ src/components/form/inputs/Input.tsx
@@ -4,23 +4,35 @@ interface InputProps {
   id: string;
   type: string;
   label: string;
+  sublabel?: string;
   placeholder: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
+  required?: boolean;
+  error?: string;
+  disabled?: boolean;
 }
 
-const Input: React.FC<InputProps> = ({ id, type, label, placeholder, onChange }) => {
+const Input: React.FC<InputProps> = ({ id, type, label, placeholder, sublabel, onChange, onBlur, required, error, disabled  }) => {
   return (
     <div className="input-wrapper">
-      <label htmlFor={id}>{label}</label>
+      <label className='label' htmlFor={id}>
+        {label}
+        {sublabel && <span className='sublabel'>{sublabel}</span>}
+      </label>
       <input 
         id={id}
         type={type} 
-        placeholder={placeholder} 
+        placeholder={placeholder}
         onChange={onChange} 
         className="input-field"
         aria-labelledby={id + "-label"}
         aria-required="true"
+        onBlur={onBlur}
+        required={required}
+        disabled={disabled}
       />
+      {error && !disabled && <p className="error" role="alert">{error}</p>}
     </div>
   );
 }
